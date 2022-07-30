98e0fa6701aebb998ef2c4d252d8aa6158137d90
Benjamin Davis <bmd-callisto@pm.me>
2022-07-29 20:42:03 -0400

    fixed env

--- config/env.go
+++ config/env.go
@@ -8,9 +8,11 @@ import (
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	return os.Getenv("MONGODB_URI")
