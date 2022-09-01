b6cd835cd44d6d2a634f19aa808d355d94019694
Benjamin Davis <davis.benjamin41902@gmail.com>
2022-09-01 15:59:46 -0400

    concurrent top

--- v1/groups/aggregation/TopCompanies.go
+++ v1/groups/aggregation/TopCompanies.go
@@ -2,48 +2,71 @@ package aggregation
 
 import (
 	"net/http"
+	"sync"
 
 	top "github.com/Matterhorn-Studios/insiderviz-forms-api/v1/utils/TopCompanies"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Top(c *gin.Context) {
 	// get the start date from the query
 	startDate := c.Query("startDate")
 
-	mostBoughtInsider, err := top.MostBought(startDate, "Insider")
+	var err error
+	var mostBoughtInsider []primitive.M
+	var mostSoldInsider []primitive.M
+	var mostBoughtCongress []primitive.M
+	var mostSoldCongress []primitive.M
+	var wg sync.WaitGroup
+	wg.Add(4)
 
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	go func() {
+		defer wg.Done()
+		mostBoughtInsider, err = top.MostBought(startDate, "Insider")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	}()
 
-	mostSoldInsider, err := top.MostSold(startDate, "Insider")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	go func() {
+		defer wg.Done()
+		mostSoldInsider, err = top.MostSold(startDate, "Insider")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	}()
 
-	mostBoughtCongress, err := top.MostBought(startDate, "Congress")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	go func() {
+		defer wg.Done()
+		mostBoughtCongress, err = top.MostBought(startDate, "Congress")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	}()
 
-	mostSoldCongress, err := top.MostSold(startDate, "Congress")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	go func() {
+		defer wg.Done()
+		mostSoldCongress, err = top.MostSold(startDate, "Congress")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	}()
+
+	wg.Wait()
 
 	returnObj := bson.M{
 		"mostBoughtInsider":  mostBoughtInsider,
